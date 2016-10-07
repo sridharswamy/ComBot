1670fac9babe0f2c8c62ff33763c4fc9a4f5cf26
akshaynayak <akshaynayak2012@gmail.com>
2016-10-07 01:30:24 -0400

    Added Github commit fetch

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const slack_token = process.env.SLACK_API_TOKEN;
 
+// Initializing Github connection
+var githubToken = "token " + process.env.GITHUB_API_TOKEN;
+var userId = "akshaynayak";
+var urlRoot = "https://api.github.com";
+var commits = []
+
 // Initializing MongoDB connection
 var mongoClient = mongodb.MongoClient;
 var mongoURL = 'mongodb://localhost:27017/slack_bot_db';
@@ -53,11 +59,85 @@ mongoClient.connect(mongoURL, function (err, db) {
         let msgText = message.text.toLowerCase();
         if(/(hello|hi|hey) (bot|sridharbot)/g.test(msgText)) {
          slack.sendMessage('Hello to you too, ' + messageSender.name + '!', channel.id);
-        }      
-  	  }	
+        }
+      
+        if(/fetch/g.test(msgText)) {
+          let dm = slack.dataStore.getDMByName(messageSender.name);
+          var re= /fetch <((https|http)\:\/\/)?((www.)?(github.com)){1}\/([\w+._]+)\/([\w+-._]+)>/g;
+          var myString = msgText;
+          var match = re.exec(myString);
+          var repoName = "";
+          var username = "";
+          if(match == null) {
+            slack.sendMessage('The URL you entered is invalid. Please enter valid GitHub URL!', channel.id);
+          }
+          else {
+            username = match[6];
+            repoName = match[7];
+            slack.sendMessage("Fetching commit history from the repo *"+repoName+"*. I will notify you as soon as I'm done!", channel.id);
+          }
+
+          commits = getCommits('sridharswamy', 'Slack-Bot', commits);
+          collection.insert({repoName:commits});
+          //console.log("Commits: " + commits);         
+          //slack.sendMessage('Commit: ' + commits, channel.id);
+        }
+      } 
     });
 
     // Start the login process
     slack.start();
+    console.log("Insertion complete");
   }
 });
+
+
+function getCommits(userName, repoName, commits)
+{
+  var options = {
+    url: urlRoot + '/repos/' + userName + '/' + repoName + '/commits',
+    method: 'GET',
+    headers: {        
+      "User-Agent": "EnableIssues",
+      "content-type": "application/json",
+      "Authorization": githubToken
+    }
+  };
+  commits = callRequest(options, commits, userName, repoName);
+  return commits;
+}
+
+function callRequest(options, commits, userName, repoName) {
+  // Send a http request to url and specify a callback that will be called upon its return.
+  request(options, function (error, response, body) 
+  {
+    var obj = JSON.parse(body);
+    commits.push(obj);
+    for( var i = 0; i < obj.length; i++ )
+    {
+      var sha = obj[i].sha;
+      var commitOptions = {
+        url: urlRoot + '/repos/' + userName + '/' + repoName + '/commits/' + sha,
+        method: 'GET',
+        headers: {        
+          "User-Agent": "EnableIssues",
+          "content-type": "application/json",
+          "Authorization": githubToken
+        }
+      };
+      callCommits(commitOptions);       
+    }
+  }); 
+  return commits;
+}
+
+function callCommits(commitOptions) {
+  request(commitOptions, function (error, response, body) 
+  {
+    var commitObj = JSON.parse(body);
+    console.log("CommitOptions" +commitObj);
+    for( var i = 0; i < commitObj.files.length; i++ ) {
+      console.log(commitObj.commit.author.name + " " + commitObj.files[i].filename);
+    }
+  });
+}
