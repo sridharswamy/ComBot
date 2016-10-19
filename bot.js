0ac5d80737708bb7293450490e9057c0b76a1362
akshaynayak <akshaynayak2012@gmail.com>
2016-10-19 01:47:53 -0400

    added mocking using nock

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ var fs = require("fs");
 var Promise = require('bluebird');
 var parse = require('parse-link-header');
 var jsonReader = require('jsonfile');
+var nock = require('nock');
 
 let parser = require('./parser.js');
 
@@ -17,10 +18,14 @@ var githubToken = "token " + process.env.GITHUB_API_TOKEN;
 var userId = "akshaynayak";
 var urlRoot = "https://api.github.com";
 var fileMappings = {};
+var data = require('./mock0.json');
+var data1 = require('./mock.json');
+
+var githubapi = nock("https://api.github.com")
 
 class Bot {
 
-  constructor(opts) {
+	constructor(opts) {
 		let slackToken = opts.token;
 		let autoReconnect = opts.autoReconnect || true;
 		let autoMark = opts.autoMark || true;
@@ -52,99 +57,57 @@ class Bot {
 		});
 
 		this.slack.start();
-  }
-
-  getCommits(userName, repoName, commits)
-	{
-	  var options = {
-	    url: urlRoot + '/repos/' + userName + '/' + repoName + '/commits',
-	    method: 'GET',
-	    headers: {        
-	      "User-Agent": "EnableIssues",
-	      "content-type": "application/json",
-	      "Authorization": githubToken
-	    }
-	  };
-	  commits = this.callRequest(options, commits, userName, repoName);
-	  return commits;
-	}
-
-	callRequest(options, commits, userName, repoName) {
-	  // Send a http request to url and specify a callback that will be called upon its return.
-	  request(options, function (error, response, body) 
-	  {
-	    var obj = JSON.parse(body);
-	    commits.push(obj);
-	    for( var i = 0; i < obj.length; i++ )
-	    {
-	      var sha = obj[i].sha;
-
-	      var commitOptions = {
-	        url: urlRoot + '/repos/' + userName + '/' + repoName + '/commits/' + sha,
-	        method: 'GET',
-	        headers: {        
-	          "User-Agent": "EnableIssues",
-	          "content-type": "application/json",
-	          "Authorization": githubToken
-	        }
-	      };
-
-				request(commitOptions, function (error, response, body) {
-					var commitObj = JSON.parse(body);
-			    for( var i = 0; i < commitObj.files.length; i++ ) {
-			      console.log(commitObj.commit.author.name + " " + commitObj.files[i].filename);
-			    }
-			  });
-	    }
-	  }); 
-	  return commits;
-	}
+  	}
 
 	messageHandler(message) {
 		let messageSender = this.slack.dataStore.getUserById(message.user);
 
-	  if(messageSender && messageSender.is_bot){
-	    return;
-	  }
+		if(messageSender && messageSender.is_bot){
+		   return;
+		}
 
-	  let channel = this.slack.dataStore.getChannelGroupOrDMById(message.channel);
+		let channel = this.slack.dataStore.getChannelGroupOrDMById(message.channel);
 		if(message.text) {
-	    let msgText = message.text.toLowerCase();
+		    let msgText = message.text.toLowerCase();
 
-	    if(/(hello|hi|hey) (bot|sridharbot)/g.test(msgText)) {
-	      this.slack.sendMessage('Hello to you too, ' + messageSender.name + '! How can I help you?\n I understand the following commands: \n 1. *fetch* [GitHub repository link] \n 2. *file* [Filename] [recent/top] [number]', channel.id);
-	    }
+		    if(/(hello|hi|hey) (bot|sridharbot)/g.test(msgText)) {
+		      this.slack.sendMessage('Hello to you too, ' + messageSender.name + '! How can I help you?\n I understand the following commands: \n 1. *fetch* [GitHub repository link] \n 2. *file* [Filename] [recent/top] [number]', channel.id);
+		    }
 
-	    if(/fetch/g.test(msgText)) {
-	      let dm = this.slack.dataStore.getDMByName(messageSender.name);
-	      var re = /fetch <((https|http)\:\/\/)?((www.)?(github.com)){1}\/([\w+._]+)\/([\w+-._]+)>/g;
+		    if(/fetch/g.test(msgText)) {
+			    let dm = this.slack.dataStore.getDMByName(messageSender.name);
+		        var re = /fetch <((https|http)\:\/\/)?((www.)?(github.com)){1}\/([\w+._]+)\/([\w+-._]+)>/g;
 
-	      if(msgText == 'fetch mock.json') {
-					var commitMessage = "";
-					parser.responseParser('mock.json', function(response) {
-						fileMappings = response;
-					});
-					this.slack.sendMessage("Fetching successfully completed! Which file do you want to query?\n Respond with the command:\n *file* [filename] [top/recent] [number]", channel.id);
+	  		    var match = re.exec(msgText);
+			    var repoName = "";
+				var username = "";
+
+				if(match == null) {
+					this.slack.sendMessage('The URL you entered is invalid. Please enter valid GitHub URL!', channel.id);
 				}
+
 				else {
-				  var match = re.exec(msgText);
-					var repoName = "";
-					var username = "";
-
-					if(match == null) {
-						this.slack.sendMessage('The URL you entered is invalid. Please enter valid GitHub URL!', channel.id);
-					}
-					else {
-					  username = match[6];
-					  repoName = match[7];
-					  this.slack.sendMessage("Fetching commit history from the repo *"+repoName+"*. I will notify you as soon as I'm done!", channel.id);
-					}
-
-					var commits = []
-					commits = this.getCommits(username, repoName, commits);
-					//console.log("Commits: " + commits);
-					//slack.sendMessage('Commit: ' + commits, channel.id);
+				  username = match[6];
+				  repoName = match[7];
+				  this.slack.sendMessage("Fetching commit history from the repo *"+repoName+"*. I will notify you as soon as I'm done!", channel.id);
 				}
+
+				var commits = []
+
+				var repocommits = githubapi.get('/repos/' + username + '/' + repoName + '/commits')
+					.reply(200, JSON.stringify(data));
+
+				var sha = data[0].sha;
+			
+				var commitresponse = githubapi.get('/repos/' + username + '/' + repoName + '/commits/' + sha)
+						.reply(200, JSON.stringify(data1));
+
+				getCommits(username, repoName, commits).then(function (response) {
+					console.log("Printing response inside callback" +response);
+					console.log(response);
+					commits = response;
+				});
+				//slack.sendMessage('Commit: ' + commits, channel.id);
 			}
 
 			if(/file/g.test(msgText)) {
@@ -166,6 +129,67 @@ class Bot {
 			}
 		}
 	}
+}
+
+function getCommits(userName, repoName, commits)
+{
+	var listOfCommits = [];
+    var options = {
+	    url: urlRoot + '/repos/' + userName + '/' + repoName + '/commits',
+	    method: 'GET',
+	    headers: {        
+	      "User-Agent": "EnableIssues",
+	      "content-type": "application/json",
+	      "Authorization": githubToken
+   		 }
+  	};
+
+   	return new Promise(function (resolve, reject) {
+	 	request(options, function (error, response, body) 
+	  	{
+		    var obj = JSON.parse(body);
+		    for(var i = 0; i < obj.length; i++)
+		    {
+		  	    var sha = obj[i].sha;
+		    	var commitOptions = {
+			        url: urlRoot + '/repos/' + userName + '/' + repoName + '/commits/' + sha,
+			        method: 'GET',
+			        headers: {        
+			          "User-Agent": "EnableIssues",
+			          "content-type": "application/json",
+			          "Authorization": githubToken
+			        }
+		      	};
+
+		      	callRequest(commitOptions, userName, repoName).then(function (commitObj) {
+		      		listOfCommits.push(commitObj);
+   					console.log("LIST OF COMMITS AFTER PUSH" +listOfCommits);
+					resolve(listOfCommits);
+		      	});
+			}
+		});
+	});		
+}
+
+function callRequest(commitOptions, userName, repoName) {
+  // Send a http request to url and specify a callback that will be called upon its return.
+
+  	return new Promise(function (resolve, reject) {
+		request(commitOptions, function (error, response, body) {
+			var commitObj = JSON.parse(body);
+		    console.log("INSIDE PROMISE commit obj"+commitObj);
+		    // for( var i = 0; i < commitObj.files.length; i++ ) {
+		    //   console.log(commitObj.commit.author.name + " " + commitObj.files[i].filename);
+		    //   console.log("Kota ki mkc");
+		    // }
+		    resolve(commitObj);
+  		});
+  	});
+  	console.log("COMMITS INSIDE PROMISE"+commits);
+    resolve(commits);
+
+
+
 
 }
 
