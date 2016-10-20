f583a71f25d1629edcc3b24767834a2d403ca8b9
Siddhant Doshi <siddoshi93@gmail.com>
2016-10-20 18:58:11 -0400

    Handled unmocked data

--- mocking.js
+++ mocking.js
@@ -7,31 +7,31 @@ var fileData = require("./mockFileData.json");
 var contributors= require("./mockContributors.json");
 var userOctocat=require("./mockUserOctocat.json");
 var userSpaceGhost=require("./mockUserSpaceGhost.json");
-
+options = {allowUnmocked: true};
 function mock(){
-	var mockServiceListCommits = nock("https://api.github.com")
+	var mockServiceListCommits = nock("https://api.github.com",options)
 	  .persist() // This will persist mock interception for lifetime of program.
 	  .get("/repos/test/Hello-World/commits")
 	  .reply(200, JSON.stringify(listOfCommits));
 
 	var mockSha=listOfCommits[0].sha
 
-	var mockServiceFileData = nock("https://api.github.com")
+	var mockServiceFileData = nock("https://api.github.com",options)
 	  .persist() // This will persist mock interception for lifetime of program.
 	  .get("/repos/test/Hello-World/commits/"+mockSha)
 	  .reply(200, JSON.stringify(fileData));
 
-	var mockContributions = nock("https://api.github.com")
+	var mockContributions = nock("https://api.github.com",options)
 	  .persist() // This will persist mock interception for lifetime of program.
 	  .get("/repos/test/Hello-World/contributors")
 	  .reply(200, JSON.stringify(contributors));
 
-	var mockUserOctocat = nock("https://api.github.com")
+	var mockUserOctocat = nock("https://api.github.com",options)
 	  .persist() // This will persist mock interception for lifetime of program.
 	  .get("/users/test")
 	  .reply(200, JSON.stringify(userOctocat));
 
-	var mockUserSpaceGhost = nock("https://api.github.com")
+	var mockUserSpaceGhost = nock("https://api.github.com",options)
 	  .persist() // This will persist mock interception for lifetime of program.
 	  .get("/users/Spaceghost")
 	  .reply(200, JSON.stringify(userSpaceGhost)); 
